2be2275f91a765e9a484c178d112f453579fb52e
Gabriel Morais <gabriel.moraisoliveira1@gmail.com>
2022-06-20 16:41:24 -0300

    fix card content export

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,6 @@
 export { default as Button } from './Button';
 export { default as Card } from './Card';
+export { default as CardContent } from './CardContent';
 export { default as Checkbox } from './Checkbox';
 export { default as CheckboxMulti } from './CheckboxMulti';
 export { default as Container } from './Container';
