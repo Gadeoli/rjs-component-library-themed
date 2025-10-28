e4371cb0112ff37675864e8b76e320c3ec965814
Gadeoli <gabriel.moraisoliveira1@gmail.com>
2025-10-28 18:20:44 -0300

    added checkbox forward ref

--- src/components/Checkbox/Checkbox.tsx
+++ src/components/Checkbox/Checkbox.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useMemo } from 'react';
+import React, { forwardRef, ForwardRefRenderFunction, useMemo } from 'react';
 import { CheckboxProps } from './Checkbox.types';
 import { 
     CheckboxSquare as StyledCheckboxSquare,
@@ -8,21 +8,25 @@ import { useTheme } from '../ThemeHandler';
 import Span from '../Span';
 import { handleCssClassnames } from '@gadeoli/js-helpers-library';
 
-const Checkbox: FC<CheckboxProps> = ({
-    name,
-    checkedValue,
-    uncheckedValue,
-    onChange,
-    size='0.75rem',
-    text,
-    type,
-    value,
-    style,
-    className,
-    disabled,
-    children,
-    checkedIcon=false
-}) => {
+const CheckboxBase : ForwardRefRenderFunction<HTMLInputElement, CheckboxProps> = (
+    {
+        name,
+        checkedValue,
+        uncheckedValue,
+        onChange,
+        size='0.75rem',
+        text,
+        type,
+        value,
+        style,
+        className,
+        disabled,
+        children,
+        checkedIcon=false,
+        ...rest
+    }, 
+    ref
+) => {
     const {theme} = useTheme();
     const colors = {
         checked: theme[type || "secondary"],
@@ -35,12 +39,25 @@ const Checkbox: FC<CheckboxProps> = ({
         className
     ]);
 
+    // ensure ref is object ref
+    const internalRef = ref as React.RefObject<HTMLInputElement>;
+
     const showCheckedIcon = useMemo(() => checkedIcon && checked && !children, [checkedIcon, checked, children]); 
 
     return (<StyledCheckboxContainer className={classNames} onClick={() => {
-        !disabled && onChange(checked ? uncheckedValue : checkedValue)
+        !disabled && onChange(checked ? uncheckedValue : checkedValue);
+        if(internalRef && internalRef?.current){
+            internalRef?.current.click();
+        }
     }} style={style}>
-        <input type="checkbox" name={name} checked={checked ? true : false} onChange={() => {}}/>
+        <input 
+            ref={internalRef} 
+            type="checkbox" 
+            name={name} 
+            checked={checked ? true : false} 
+            {...rest}
+            onChange={() => {}}//keep onchange after rest //override rest onchange //not allowed to change 
+        />
 
         <StyledCheckboxSquare 
             className={`cl-themed__checkbox__square ${disabled ? 'disabled' : ''}`} 
@@ -57,6 +74,8 @@ const Checkbox: FC<CheckboxProps> = ({
     
     </StyledCheckboxContainer>)
 }
+
+export const Checkbox = forwardRef(CheckboxBase);
 
 export default Checkbox;
 
